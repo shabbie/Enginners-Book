f92d593e940f7b094583af38b7e4a9c28a77ec4f
abhi2496 <abhishekkoranne2@gmail.com>
2017-12-24 12:17:00 +0530

    Add files via upload

---
+++ ChatFragment.java
@@ -0,0 +1,34 @@
+package com.example.abhishekkoranne.engineersbook.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.abhishekkoranne.engineersbook.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChatFragment extends Fragment {
+
+
+    public ChatFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+}
