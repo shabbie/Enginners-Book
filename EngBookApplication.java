b4fd1e25cf88c39f0832e3f5f701b7a4c462b6fd
abhi2496 <abhishekkoranne2@gmail.com>
2017-12-24 12:15:18 +0530

    Add files via upload

---
+++ EngBookApplication.java
@@ -0,0 +1,12 @@
+package com.gtu.EngBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngBookApplication.class, args);
+	}
+}
