4e6dc4ec02321a655d23a4cc033867dce9d92cca
Abhishek Koranne <abhishekkoranne2@gmail.com>
2018-03-04 09:18:12 +0530

    Add files via upload

---
+++ Engineer-s Book/app/src/main/AppEngineersBook.java
@@ -0,0 +1,21 @@
+package com.example.abhishekkoranne.engineersbook;
+
+import android.app.Application;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+/**
+ * Created by Abhishek Koranne on 09-02-2018.
+ */
+
+public class AppEngineersBook extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Create global configuration and initialize ImageLoader with this config
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this).build();
+        ImageLoader.getInstance().init(config);
+    }
+}
